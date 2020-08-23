4fd24417601a33190cbd115b09d59cfc8ac84be8
steven3092 <steven.fauterre@laposte.net>
2020-08-23 13:36:06 +0200

    cmd

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist/gifmash')));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist/gifmash', 'index.html'));
 }); app.listen(port);
